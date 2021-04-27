c37316ccbe98f56f9938b8d02408d88314b7ec13
pulga23 <79906638+pulga23@users.noreply.github.com>
2021-04-27 22:51:02 +0100

    time(minutes:seconds)

--- Assets/Scripts/Geral.cs
+++ Assets/Scripts/Geral.cs
@@ -11,22 +11,36 @@ public class Geral : MonoBehaviour
     [SerializeField]
     private Text timeText; //time the player has left to show on screen
 
+    private bool timeRunning=true;
+
+    private float minutes;
+    private float seconds;
+
     // Update is called once per frame
     void Update()
     {
-  
-        timeLeft -= Time.deltaTime; //contdown the time the player has left
-        timeText.text = timeLeft.ToString(); //print the time to the game screen       
-        if( timeLeft <= 0f) //when time is 0 
+        if(timeLeft>0 && timeRunning==true) //cheack if there is time left
+        {
+            timeLeft -= Time.deltaTime; //contdown the time the player has left
+           
+        }
+        //convert time to format minutes:seconds
+        minutes = Mathf.FloorToInt(timeLeft / 60); //multiply per 60 to get the minutes
+        seconds = Mathf.FloorToInt(timeLeft % 60); //modular operation to get the seconds
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds); //print the time to the game screen       
+        
+        if( timeLeft <= 0f && timeRunning == true) //when time is 0 
         {
             //Game Over
+            timeRunning = false; //bool variable to make it only run once
+            timeLeft = 0f;
             GameOver();
         }
     }
 
     private void GameOver() //actions to run when the player dies
     {
-        timeText.text = 0.ToString();  
+        
         Debug.Log("Game Over!)");
     }
 }
